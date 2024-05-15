d1b8c6632c9de1c8fdee373baf4c690c1ee62718
btdev0306 <btdev0306@outlook.com>
2024-05-15 14:00:12 -0400

    addvalidator

---
+++ SolanaProgram/localsolanavalidator.js
@@ -0,0 +1,8 @@
+const solana=require("@solana/web3.js");
+const connection=new solana.Connection('http://127.0.0.1:8899','confirmed');
+(async()=>{
+    const version=await connection.getVersion();
+    console.log('Connection to cluster established:',version);
+    const epoch=await connection.getEpochInfo();
+    console.log('Epoch info',epoch)
+})();
